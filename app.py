2a5d4cac7b3e09acc7a5ee8326447cc3f2613018
Karan Goel <karan@goel.im>
2013-10-29 00:01:03 -0700

    Better docs

--- app.py
+++ app.py
@@ -7,20 +7,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<b>Check out <a href=\"https://github.com/thekarangoel/HNify\">HNify on Github</a>.</b>"
+    '''
+    This page is displayed when index page is requested.
+    '''
+    return '''<b>Check out <a href="https://github.com/thekarangoel/HNify">HNify on Github</a>.</b>'''
 
 @app.route('/get/top', methods = ['GET'])
 def get_top():
+    '''
+    Returns stories from the front page of HN.
+    '''
     hn = HN()
     return jsonify({'stories': hn.get_stories()})
 
 @app.route('/get/<story_type>', methods = ['GET'])
 def get_stories(story_type):
+    '''
+    Returns stories from the requested page of HN.
+    story_type is one of:
+    \tnewest
+    \tbest
+    '''
     hn = HN()
     return jsonify({'stories': hn.get_stories(story_type=story_type)})
 
 @app.errorhandler(404)
 def not_found(error):
+    '''
+    Returns a jsonified 404 error message instead of a HTTP 404 error.
+    '''
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 if __name__ == '__main__':
